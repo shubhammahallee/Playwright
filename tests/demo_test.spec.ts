e0b4f0f09c1aeae1ff13700b5408d32662f33047
Shubham Mahalle <shubhammahallee@gmail.com>
2026-08-23 12:22:17 +0530

    Update demo_test.spec.ts

--- tests/demo_test.spec.ts
+++ tests/demo_test.spec.ts
@@ -44,7 +44,7 @@ test('Search Testing', async ({ page }) => {
 
     // Click first video result (more robust than long title)
     await page.locator("//a[@title='♥︎ Heiße großbrüstige Brünette züchten und sie zum Schreien ♥bringen ︎ Creampie nach HARTEM Missionar ohne Verhütung - Candy Love POV']").first().click();
-    await page.locator("//button[contains(text(), 'Werbung')]").click();
+    await page.locator("//button[contains(text(), 'Werbung')]").click(); 
 
 
     await page.waitForLoadState('networkidle');
