6059c7d91af92b06248111ea20c1f0ae3c13c148
Shubham Mahalle <shubhammahallee@gmail.com>
2026-08-25 09:28:11 +0530

    Update demo.spec.ts

--- tests/demo.spec.ts
+++ tests/demo.spec.ts
@@ -12,7 +12,7 @@ test('Basic Demo Test', async ({ page }) => {
     await page.locator('//select[@id="dropdown-class-example"]').selectOption('Option3');
     await page.getByLabel('Option3').check();
     await page.locator("//label[@for='male']").click();
-
+ 
     await page.locator("//a[@class='orangeButton']//i[@class='ph-icon-chevron-right']").click();
     await page.screenshot({ path: "./screenshots/screenshot.png" });
 
